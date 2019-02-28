37e0db27b5ae53dcdcbe3103f1a5ac113f5c04e6
Darryl <darrylmangibin@gmail.com>
2019-02-28 22:44:39 +0800

    add spinner

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
+  state = {
+    lat: null,
+    errorMessage: ''
+  }
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        // update state
         this.setState({
           lat: position.coords.latitude
         });
@@ -21,7 +22,11 @@ class App extends React.Component {
         });
       }
     );
-  };
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!');
+  }
 
   // React says we have to define render!!
   render() {
@@ -29,10 +34,12 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if(!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay 
+        lat={this.state.lat}
+      />
     }
     if(!this.state.errorMessage && !this.state.lat) {
-      return <div>Loading...</div>
+      return <Spinner/>
     }
   };
 };
