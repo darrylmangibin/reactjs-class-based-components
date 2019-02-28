d627507e27f0c075f5c1a6026d9f98d67adda464
Darryl <darrylmangibin@gmail.com>
2019-02-28 18:09:12 +0800

    updating State Properties

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null
+    };
+  };
+
+  // React says we have to define render!!
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // update state
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        console.log(err)
+      }
+    );
+    return(
+      <div>
+        Latitude: {this.state.lat}
+      </div>
+    );
+  };
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
