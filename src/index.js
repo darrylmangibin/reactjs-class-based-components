ba8fa93d707f07356b7acd97bd21e816e4366e63
Darryl <darrylmangibin@gmail.com>
2019-02-28 18:39:37 +0800

    conditional rendering content

--- src/index.js
+++ src/index.js
@@ -5,26 +5,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ''
     };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // update state
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    );
   };
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // update state
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        console.log(err)
-      }
-    );
-    return(
-      <div>
-        Latitude: {this.state.lat}
-      </div>
-    );
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    if(!this.state.errorMessage && !this.state.lat) {
+      return <div>Loading...</div>
+    }
   };
 };
 
